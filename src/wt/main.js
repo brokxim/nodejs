e75d069c49c18fd4e53dea42690356b69f6307d5
ibrohimsoft <ahatkulov98@gmail.com>
2024-01-30 15:36:11 +0500

    update file (deleted wrong console msg)

--- src/wt/main.js
+++ src/wt/main.js
@@ -6,19 +6,15 @@ const performCalculations = async () => {
   let result,
     allResult = [];
   for (let i = 0; i < cpuCount; i++) {
-    const worker = new Worker("./worker.js");
+    const worker = new Worker("./worker.js", { workerData: {} });
     worker.postMessage(numToWorker);
     worker.on("message", (data) => {
-      result = data;
-      console.log("result", result);
+      result = { status: "resolved", data: data };
 
       allResult.push(result);
     });
     worker.on("error", (err) => console.log(`status : error, data :${null}`));
     numToWorker++;
   }
-  console.log("allResult", allResult);
 };
 await performCalculations();
-
-// result = { status: "resolved", data: data };
